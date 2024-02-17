394127a138c99e7c34da431f876fa6e3b1ec7d2b
s.sufi <saeed.sufi@hotmail.com>
2024-02-17 14:42:18 +0330

    Replaced mongodb connection with postgres

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
+require("dotenv").config()
+const { Pool } = require('pg')
 const app = express();
 
 // middleware
@@ -10,11 +10,29 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
+const pool = new Pool({
+  connectionString
+})
+
+const createStoreTable = async () => {
+  try {
+    await pool.query(
+      'CREATE TABLE IF NOT EXISTS store (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR (255) NOT NULL, slug VARCHAR (255) UNIQUE NOT NULL, description VARCHAR (1000), tags TEXT [], location POINT, address VARCHAR (1000))')
+  } catch (error) {
+    throw (error)
+  }
+}
+
+pool.connect((err, data) => {
+  if (err) { throw new Error }
+  else {
+    // createStoreTable()
+    app.listen(3000)
+    console.log('listening on port 3000...')
+  }
+})
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
---
+++ dbConfig.js
@@ -0,0 +1,6 @@
+require("dotenv").config()
+const { Pool } = require('pg')
+
+
+// createStoreTable()
+module.exports = pool 
